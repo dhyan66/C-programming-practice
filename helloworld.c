d7047c7fd47b11bb18ed695710b01fdbe11fdc14
dhyan66 <94802145+dhyan66@users.noreply.github.com>
2024-02-04 14:55:38 -0400

    Day 2 of practice

--- helloworld.c
+++ helloworld.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 // function prototypes checks if the correct arguments are passed to the function something like interface in java
 void birthday(char [] ,int);
 // functions
@@ -17,7 +18,7 @@ void birthday(char name[],int age) {
 
 
 int main()
-{
+ {
     // this is a comment blah
     /*
      * this is a multi line comment.
@@ -55,8 +56,39 @@ int main()
     if(x<=3||x>0&&x<90||x!=90);
 
 
+    // string functions
+     strlwr(name); // lower case in strings
+     strupr(name); // upper case the strings
+     strcat(name, name); // appends the name to name
+     strncat(name,name,1); // appends the 1 char from name to name
+     strcpy(name,name); // copies first 2nd to 1st name
+     strncpy(name,name,2); // copies just 2 chars
+     strlen(name);// returns the length of the string
 
+     // do while loop check if a condition is true if its true then the loop will run again else terminate
 
+     int number = 0;
+     int sum = 0;
+     do{
+         printf("Enter a number above 0:");
+         number++;
+     }
+     while(number>0);
+
+     // continue statement skips the code section and forces the next iteration of the code
+
+     for(int i = 1; i<=20; i++){
+         if(i == 13){
+             continue; // this continue statement skips the print statement when i == 13
+         }
+         printf("%d",i);
+     }
+     // size of operator array
+     double prices [] ={5.0,10.0,9.0};
+    printf("%1f",sizeof(prices));
+    for(int i =0; i<sizeof(prices)/sizeof (prices[0]);i++){
+        printf("$%.21f\n",prices[i]);
+    }
 
     return 0;
 }
